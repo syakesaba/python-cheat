7273fd00dec7f014c8a2efb94841be29602882fc
syakesaba <syakesaba@gmail.com>
2015-08-22 21:30:54 +0900

    連続データのメモリダンプ。差分メモリダンプとしては使えないかなあ

--- cheat/memory.py
+++ cheat/memory.py
@@ -31,3 +31,49 @@ class Memory(object):
         for mp in self.maps:
             if mp.pathname == pathname:
                 return mp
+
+class SequentialMemoryDump(object):
+    def __init__(self, start_address, end_address, dump_data):
+        assert(type(start_address) == int)
+        assert(type(end_address) == int)
+        self.start_address = start_address
+        self.end_address = end_address
+        self.dump_data = dump_data
+
+    def __lt__(self, other_memdump):
+        assert(type(other_memdump) == type(self))
+        pass
+
+    def __le__(self, other_memdump):
+        assert(type(other_memdump) == type(self))
+        pass
+
+    def __eq__(self, other_memdump):
+        assert(type(other_memdump) == type(self))
+        pass
+
+    def __ne__(self, other_memdump):
+        assert(type(other_memdump) == type(self))
+        pass
+
+    def __gt__(self, other_memdump):
+        assert(type(other_memdump) == type(self))
+        pass
+
+    def __ge__(self, other_memdump):
+        assert(type(other_memdump) == type(self))
+        pass
+
+    def __str__(self):
+        return self.dump_data
+
+    def __repr__(self):
+        return "%s - %s %d bytes" % (hex(self.start_address), hex(self.end_address), len(self.dump_data))
+
+    def bitmap(self):
+        #BZdump
+        pass
+
+    def magic(self):
+        #file command magic bytes detection
+        pass
